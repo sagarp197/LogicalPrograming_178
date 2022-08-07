75730f989642e3c98a1e0ea4bc7a893d9685ed83
sagarp197 <sagarp197@gmail.com>
2022-08-08 03:59:14 +0530

    code added for Perfect Number

---
+++ PerfectNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+
+        public static void DisplayPerfectNumber()
+        {
+            int n, i, sum;
+            int min, max;
+
+
+            Console.WriteLine("Find perfect numbers within a given number of range");
+
+
+            Console.WriteLine("Input the starting range or number : ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the ending range of number : ");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Perfect numbers within the given range : ");
+            for (n = min; n < max; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0} ", n);
+            }
+
+
+
+        }
+    }
+}
