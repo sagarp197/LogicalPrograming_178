fdb2520869676c7934c52e07e80898d23604ebf6
sagarp197 <sagarp197@gmail.com>
2022-08-08 03:52:59 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FibonacciSeries.DisplayFibonacciSeries();
+            //PerfectNumber.DisplayPerfectNumber();
+            //PrimeNumber.DisplayPrimeNumber();
+            ReverseNumber.DisplayReverseNumber();
+            //CouponNumbers.CouponNumbersGenerator();
+        }
+    }
+}
