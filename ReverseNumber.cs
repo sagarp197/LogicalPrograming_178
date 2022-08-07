a8a8b0e43121fc8c31dc09bf468bcdc0ba014b31
sagarp197 <sagarp197@gmail.com>
2022-08-08 04:14:02 +0530

    code added for Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class ReverseNumber
+    {
+        public static void DisplayReverseNumber()
+        {
+
+            int n, reverse = 0, rem;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
